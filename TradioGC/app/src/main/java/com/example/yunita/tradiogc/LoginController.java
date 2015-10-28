e7bf43a6f342b71cf318969fd32eba4dca3d418e
ayunita <yunita@ualberta.ca>
2015-10-27 21:08:08 -0600

    adding signUp_ui

---
+++ TradioGC/app/src/main/java/com/example/yunita/tradiogc/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.yunita.tradiogc;
+
+import android.content.Context;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+
+/**
+ * Created by yunita on 27/10/15.
+ */
+public class LoginController {
+
+    public LoginController(){}
+
+}
--- TradioGC/app/src/main/java/com/example/yunita/tradiogc/MainActivity.java
+++ TradioGC/app/src/main/java/com/example/yunita/tradiogc/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.yunita.tradiogc;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+
+    private Context mContext = this;
+
+    private LinearLayout login_view;
+    private LinearLayout signup_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        login_view = (LinearLayout) findViewById(R.id.login_view);
+        signup_view = (LinearLayout) findViewById(R.id.signUp_view);
+
     }
 
+    public void goToSignUp(View view){
+        Animation anim = AnimationUtils.loadAnimation(mContext,R.anim.pull_down);
+        login_view.setVisibility(View.GONE);
+        signup_view.setVisibility(View.VISIBLE);
+        signup_view.startAnimation(anim);
+    }
+
+    public void goToLogin(View view){
+        signup_view.setVisibility(View.GONE);
+        login_view.setVisibility(View.VISIBLE);
+    }
 
 }
