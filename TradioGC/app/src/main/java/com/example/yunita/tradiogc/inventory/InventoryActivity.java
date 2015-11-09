d58adb5271328d40b9afd33feac294e66b5a1c78
ayunita <yunita@ualberta.ca>
2015-11-09 00:42:07 -0700

    working on inventory bug

--- TradioGC/app/src/main/java/com/example/yunita/tradiogc/inventory/InventoryActivity.java
+++ TradioGC/app/src/main/java/com/example/yunita/tradiogc/inventory/InventoryActivity.java
@@ -18,40 +18,92 @@ import com.example.yunita.tradiogc.user.UserController;
 public class InventoryActivity extends AppCompatActivity {
     private Context context = this;
     private Inventory inventory = LoginActivity.USERLOGIN.getInventory();
+    private String targetUsername;
+
     private ListView itemList;
     private ArrayAdapter<Item> inventoryViewAdapter;
+    private Button add_item_button;
+
     private InventoryController inventoryController;
+    private UserController userController;
+    private Runnable doUpdateGUIDetails = new Runnable() {
+        public void run() {
+            inventoryViewAdapter.clear();
+            inventoryViewAdapter.addAll(inventory);
+            inventoryViewAdapter.notifyDataSetChanged();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.your_inventory);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
         inventoryController = new InventoryController(context);
+        userController = new UserController(context);
+
         itemList = (ListView) findViewById(R.id.inventory_list_view);
+        add_item_button = (Button) findViewById(R.id.add_item_button);
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         inventoryViewAdapter = new ArrayAdapter<Item>(this, R.layout.inventory_list_item, inventory);
         itemList.setAdapter(inventoryViewAdapter);
 
-        itemList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Item deletedItem = inventory.get(position);
-                Thread deleteThread = inventoryController.new DeleteItemThread(deletedItem);
-                deleteThread.start();
-                Toast.makeText(context, "Removing " + deletedItem.toString(), Toast.LENGTH_SHORT).show();
-                inventoryViewAdapter.notifyDataSetChanged();
-                return true;
+        Intent intent = getIntent();
+        if (intent != null) {
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                targetUsername = extras.getString("user_inventory");
             }
-        });
+        }
+
+        // check whether this is my profile or not
+        // if it is friend's inventory, then cannot remove and add item into inventory.
+        if (!targetUsername.equals(LoginActivity.USERLOGIN.getUsername())) {
+            // friend's inventory
+            add_item_button.setVisibility(View.GONE);
+
+//            // BUG! need to be fixed, or other solution: separate the friend and my inventory
+//            Thread getUserThread = new GetThread(targetUsername);
+//            getUserThread.start();
+
+        } else {
+            // my inventory
+            itemList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    Item deletedItem = inventory.get(position);
+                    Thread deleteThread = inventoryController.new DeleteItemThread(deletedItem);
+                    deleteThread.start();
+                    Toast.makeText(context, "Removing " + deletedItem.toString(), Toast.LENGTH_SHORT).show();
+                    inventoryViewAdapter.notifyDataSetChanged();
+                    return true;
+                }
+            });
+        }
+
     }
 
-    public void goToAddItem(View view){
+    public void goToAddItem(View view) {
         startActivity(new Intent(InventoryActivity.this, AddItemActivity.class));
+    }
+
+    class GetThread extends Thread {
+        private String username;
+
+        public GetThread(String username) {
+            this.username = username;
+        }
+
+        @Override
+        public void run() {
+            inventory = userController.getUser(username).getInventory();
+            runOnUiThread(doUpdateGUIDetails);
+
+        }
     }
 
 }
