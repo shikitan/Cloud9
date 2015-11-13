4a86ad83ced59100c15746542e896e678dc41563
shuming2 <Amber@ZhangShumingdeMacBook-Air.local>
2015-11-12 22:29:44 -0700

    fix search size problem

--- TradioGC/app/src/main/java/com/example/yunita/tradiogc/WebServer.java
+++ TradioGC/app/src/main/java/com/example/yunita/tradiogc/WebServer.java
@@ -2,7 +2,7 @@ package com.example.yunita.tradiogc;
 
 public class WebServer {
     private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/inv/";
-    private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/inv/_search";
+    private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/inv/_search?size=1000000";
 
     public WebServer() {
 
