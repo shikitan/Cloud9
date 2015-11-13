2885cc8593123a30745798e88cb2ae52d96359c5
ayunita <yunita@ualberta.ca>
2015-11-12 23:10:32 -0700

    Updating readme

--- TradioGC/app/src/main/java/com/example/yunita/tradiogc/WebServer.java
+++ TradioGC/app/src/main/java/com/example/yunita/tradiogc/WebServer.java
@@ -2,8 +2,12 @@ package com.example.yunita.tradiogc;
 
 public class WebServer {
     private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/inv/";
+    // specify the limit size of elastic search (its default = 10)
     private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/inv/_search?size=1000000";
 
+    /**
+     * Class constructor.
+     */
     public WebServer() {
 
     }
